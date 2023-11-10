0e370a626ffbd67f9ce1ee10da678d9c904ef163
Zhihao Tang <tzhtaylor@163.com>
2023-11-10 14:15:55 +0800

    fix sql injection and add resource leak

--- src/main/java/com/ResourceLeakExample.java
+++ src/main/java/com/ResourceLeakExample.java
@@ -5,21 +5,21 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ResourceLeakExample {
-//    public static void main(String[] args) {
-//        BufferedReader reader = null;
-//        try {
-//            reader = new BufferedReader(new FileReader("test.txt"));
-//            String firstLine = reader.readLine();
-//            System.out.println(firstLine);
-//            // The reader is not closed in this execution path
-//            if (firstLine == null) {
-//                return;
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        // Resource leak: 'reader' is never closed on normal or exceptional execution paths
-//    }
+   public static void main(String[] args) {
+       BufferedReader reader = null;
+       try {
+           reader = new BufferedReader(new FileReader("test.txt"));
+           String firstLine = reader.readLine();
+           System.out.println(firstLine);
+           // The reader is not closed in this execution path
+           if (firstLine == null) {
+               return;
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+       // Resource leak: 'reader' is never closed on normal or exceptional execution paths
+   }
     
     
 //    public static void main(String[] args) {
@@ -33,11 +33,11 @@ public class ResourceLeakExample {
 //        // 不需要显式关闭BufferedReader，try-with-resources会自动处理
 //    }
 	
-	public static void main(String[] args) {
-		String password;
-        String str = "12345";
-        password = str; 
-        System.out.println(password);
-    }
+	// public static void main(String[] args) {
+	// 	String password;
+    //     String str = "12345";
+    //     password = str; 
+    //     System.out.println(password);
+    // }
 
 }
